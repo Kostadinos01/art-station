4fbb3f14d99cb8562f5159cb0dad0d95dffff397
Kostadinos01 <kostasigna@yahoo.gr>
2023-03-08 16:07:29 +0200

    updated

--- src/components/Input/style.ts
+++ src/components/Input/style.ts
@@ -1,4 +1,28 @@
 import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+
+const CustomButton = styled(Button)({
+  width: "140px",
+  height: "50px",
+  cursor: "pointer",
+  border: "1px solid #3fe0d0",
+  borderRadius: "50px",
+  backgroundColor: "#3fe0d0",
+  outline: "none",
+  transition: "0.8s ease-in-out",
+  color: "white",
+  fontSize: "16px",
+  fontWeight: "bolder",
+  display: "flex",
+  margin: "auto",
+  justifyContent: "center",
+  "&:hover": {
+    transition: "0.8s ease-in-out",
+    backgroundColor: "white",
+    border: "1px solid white",
+    color: "#3fe0d0",
+  },
+});
 
 const Container = styled("div")({
   border: "solid 3px white",
@@ -20,11 +44,23 @@ const CustomInput = styled("input")({
   border: "solid 3px #3fe0d0",
   borderRadius: "50px",
   backgroundColor: "#3fe0d0",
-  width: "950px",
-  height: "35px",
-  fontSize: "15px",
+  width: "100%",
+  height: "2.5em",
+  fontSize: "1rem",
   outline: "none",
   fontWeight: "bolder",
+  "@media (max-width: 1200px)": {
+    width: "100%",
+  },
+  "@media (max-width: 1024px)": {
+    width: "75%",
+  },
+  "@media (max-width: 768px)": {
+    width: "75%",
+  },
+  "@media (max-width: 480px)": {
+    width: "75%",
+  },
 });
 
 const CustomLabel = styled("label")({
@@ -38,4 +74,4 @@ const CustomLabel = styled("label")({
   marginTop: "30px",
 });
 
-export { CustomInput, CustomLabel, Container };
+export { CustomInput, CustomLabel, Container, CustomButton };
--- src/components/RenderedImage/style.ts
+++ src/components/RenderedImage/style.ts
@@ -1,9 +1,25 @@
 import { styled } from "@mui/material/styles";
 
 const CustomImage = styled("img")({
-  width: "400px",
+  width: "400",
   height: "400px",
   marginTop: "50px",
+  "@media (max-width: 1200px)": {
+    width: "400",
+    height: "400px",
+  },
+  "@media (max-width: 1024px)": {
+    width: "400",
+    height: "400px",
+  },
+  "@media (max-width: 768px)": {
+    width: "300",
+    height: "300px",
+  },
+  "@media (max-width: 480px)": {
+    width: "300",
+    height: "300px",
+  },
 });
 
 export { CustomImage };
